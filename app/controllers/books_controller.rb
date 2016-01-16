845c3a4a26e86a30752c4543fb99a4e602d93370
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-16 14:12:09 +0000

    Add create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,20 @@ class BooksController < ApplicationController
   def new 
     @book = Book.new
   end
+  
+  def create
+    @book = Book.create(book_params)
+    if @book.save
+      flash[:success] = "Book Added."
+      redirect_to @book
+    else
+      flash[:error] = "There was an error."
+      render :new
+    end
+  end
+  
+  private
+    def book_params
+      params.require(:book).permit(:title, :author)
+    end
 end
