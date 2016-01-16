5f9102b9f5c127e155efc9f0e778d4784b79b994
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-16 13:53:40 +0000

    Add associations to models, create tables books and reviews

--- db/migrate/20160116135032_create_reviews.rb
+++ db/migrate/20160116135032_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+      t.string :name
+      t.text :comment
+      t.references :book
       t.timestamps null: false
     end
   end
