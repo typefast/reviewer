4eb606617ef360615a1e877f47b77642fed33d4c
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-16 17:25:12 +0000

    Add create action ro ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,21 @@ class ReviewsController < ApplicationController
   def new
     @book = Book.find(params[:book_id])
   end
+  
+  def create
+    @book = Book.find(params[:book_id])
+    @review = @book.review.create(review_params)
+    if @review.save
+      flash[:success] = "Review Added."
+      redirect_to @book_path
+    else
+      flash[:error] = "There was an error."
+      render :new
+    end
+  end
+  
+  private
+    def review_params
+      params.require(:review).permit(:name, :comment)
+    end
 end
